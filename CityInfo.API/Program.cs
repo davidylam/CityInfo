7982c41241647a79e73e172d464fcff16df645cf
David Lam <David.Lam@DecisionResearch.com>
2024-03-21 08:59:54 -1000

    Completed Module 3. Creating the API... Next Manipulating Resources and Validating Input

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,11 +1,29 @@
+using Microsoft.AspNetCore.StaticFiles;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+
+builder.Services.AddControllers(options =>
+{
+    options.ReturnHttpNotAcceptable = true;
+}).AddXmlDataContractSerializerFormatters();
+
+builder.Services.AddProblemDetails(options =>
+{
+    options.CustomizeProblemDetails = ctx =>
+    {
+        ctx.ProblemDetails.Extensions.Add("additionalInfo", "Additional info example");
+        ctx.ProblemDetails.Extensions.Add("server", Environment.MachineName);
+    };
+});
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
 
 var app = builder.Build();
 
