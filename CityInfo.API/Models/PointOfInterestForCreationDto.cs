404510aeb1ae4ebe156d2fb7349284950c65753c
David Lam <David.Lam@DecisionResearch.com>
2024-03-21 13:53:04 -1000

    Completed - Validating Input

--- CityInfo.API/Models/PointOfInterestForCreationDto.cs
+++ CityInfo.API/Models/PointOfInterestForCreationDto.cs
@@ -1,7 +1,12 @@
-﻿namespace CityInfo.API.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Models;
 
 public class PointOfInterestForCreationDto
 {
+    [Required(ErrorMessage ="you should provide a name value")]
+    [MaxLength(50)]
     public string Name { get; set; } = string.Empty;
+    [MaxLength(200)]
     public string? Description { get; set; } 
 }
