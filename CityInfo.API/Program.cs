032e9e4c7c1d12fde2272b520419c76245ca99cc
David Lam <David.Lam@DecisionResearch.com>
2024-03-25 15:45:51 -1000

    Replace Default Logger and Log to File

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,8 +1,16 @@
 using Microsoft.AspNetCore.StaticFiles;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("logs/cityinfo.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Logging.ClearProviders();
-builder.Logging.AddConsole();
+//builder.Logging.ClearProviders();
+//builder.Logging.AddConsole();
+builder.Host.UseSerilog();
 
 // Add services to the container.
 
