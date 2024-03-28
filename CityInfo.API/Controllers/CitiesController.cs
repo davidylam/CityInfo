8a3570af2e6f4c897dc1a00a6040389ff2a3abfd
David Lam <David.Lam@DecisionResearch.com>
2024-03-28 10:33:55 -1000

    Use Automapper

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -1,4 +1,5 @@
-﻿using CityInfo.API.Models;
+﻿using AutoMapper;
+using CityInfo.API.Models;
 using CityInfo.API.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,27 +11,19 @@ namespace CityInfo.API.Controllers;
 public class CitiesController : ControllerBase
 {
     private readonly ICityInfoRepository _cityInfoRepository;
+    private readonly IMapper _mapper;
 
-    public CitiesController(ICityInfoRepository cityInfoRepository)
+    public CitiesController(ICityInfoRepository cityInfoRepository, IMapper mapper)
     {
         _cityInfoRepository = cityInfoRepository ?? throw new ArgumentNullException(nameof(cityInfoRepository));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper)); ;
     }
     [HttpGet]
     public async Task<ActionResult<IEnumerable<CityDto>>> GetCities()
     {
         var cityEntities = await _cityInfoRepository.GetCitiesAsync();
 
-        var results = new List<CityWithoutPointsOfInterestDto>();
-        foreach (var cityEntity in cityEntities)
-        {
-            results.Add(new CityWithoutPointsOfInterestDto { 
-                 Id = cityEntity.Id,
-                 Name = cityEntity.Name,
-                 Description = cityEntity.Description
-            });
-        }
-        return Ok(results);
-       
+        return Ok(_mapper.Map<IEnumerable<CityWithoutPointsOfInterestDto>>(cityEntities));
     }
 
     [HttpGet("{id}")]
---
+++ CityInfo.API/Profiles/CityProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+
+namespace CityInfo.API.Profiles
+{
+    public class CityProfile: Profile
+    {
+
+        public CityProfile()
+        {
+            CreateMap<Entities.City, Models.CityWithoutPointsOfInterestDto>();
+        }
+
+
+    }
+}
