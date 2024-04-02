835effc5de00d54e93d763fd5d1b1e06f77a1429
David Lam <David.Lam@DecisionResearch.com>
2024-04-02 09:58:56 -1000

    Searching, Filtering and Paging Resources

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -2,6 +2,7 @@
 using CityInfo.API.Models;
 using CityInfo.API.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace CityInfo.API.Controllers;
 
@@ -12,6 +13,7 @@ public class CitiesController : ControllerBase
 {
     private readonly ICityInfoRepository _cityInfoRepository;
     private readonly IMapper _mapper;
+    const int maxCitiesPageSize = 20;
 
     public CitiesController(ICityInfoRepository cityInfoRepository, IMapper mapper)
     {
@@ -19,9 +21,18 @@ public class CitiesController : ControllerBase
         _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper)); ;
     }
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<CityDto>>> GetCities()
+    public async Task<ActionResult<IEnumerable<CityDto>>> GetCities(string? name, string? searchQuery, int pageNumber = 1, int pageSize = 10)
     {
-        var cityEntities = await _cityInfoRepository.GetCitiesAsync();
+        if (pageSize > maxCitiesPageSize)
+        {
+            pageSize = maxCitiesPageSize;
+        }
+
+        var (cityEntities, paginationMetadata) = await _cityInfoRepository
+            .GetCitiesAsync(name, searchQuery, pageNumber, pageSize);
+
+        Response.Headers.Add("X-Pagination",
+            JsonSerializer.Serialize(paginationMetadata));
 
         return Ok(_mapper.Map<IEnumerable<CityWithoutPointsOfInterestDto>>(cityEntities));
     }
